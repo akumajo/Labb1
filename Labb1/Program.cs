091558fee4c7c10891fa76273f47c522d94cfb02
Fredric <Fredric@DESKTOP-OJT2DFD>
2020-09-14 10:08:58 +0200

    Test

---
+++ Labb1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder addDigitsToString = new StringBuilder();
+            string userInput = Console.ReadLine();
+            long countSum = 0;
+
+            for (int y = 0; y < userInput.Length - 1; y++)
+            {
+                if (!char.IsDigit(userInput[y]))
+                {
+                    continue;
+                }
+
+                addDigitsToString.Clear();
+                addDigitsToString.Append(userInput[y]);
+
+                for (int x = y + 1; x < userInput.Length; x++)
+                {
+                    if (!char.IsDigit(userInput[x]))
+                    {
+                        break;
+                    }
+
+                    addDigitsToString.Append(userInput[x]);
+
+                    if (userInput[y] == userInput[x])
+                    {
+                        break;
+                    }
+                }
+
+                if (addDigitsToString[0] == addDigitsToString[addDigitsToString.Length - 1] && addDigitsToString.Length > 1)
+                {
+                    Console.Write(userInput.Remove(y));
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write(addDigitsToString);
+                    Console.ResetColor();
+                    Console.Write(userInput.Substring(y + addDigitsToString.Length));
+
+                    Console.WriteLine();
+                    countSum += long.Parse(addDigitsToString.ToString());
+                }
+            }
+            Console.WriteLine($"Summan av kardemumman: {countSum}");
+        }
+    }
+}
